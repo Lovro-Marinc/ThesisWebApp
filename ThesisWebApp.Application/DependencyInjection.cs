94b4a8249db3ca36db2ff1dff3643628db3784a8
Lokatian <lovromarinc123@gmail.com>
2025-11-22 01:09:32 +0100

    Architecture

---
+++ ThesisWebApp.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThesisWebApp.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Register application services here
+        return services;
+    }
+}

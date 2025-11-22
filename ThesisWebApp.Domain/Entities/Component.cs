1decac60ea5dfa9a6956a9504b223ca80b8deddc
Lokatian <lovromarinc123@gmail.com>
2025-11-22 20:23:06 +0100

    Fixed component

--- ThesisWebApp.Domain/Entities/Component.cs
+++ ThesisWebApp.Domain/Entities/Component.cs
@@ -1,24 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ThesisWebApp.Domain.Entities;
-
+[Table("Component", Schema = "dbo")]
 public class Component
 {
+    [Key]
     public int ComponentID { get; set; }
-    public int SectionID { get; set; }
-    public string Position { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+
+    [ForeignKey("SectionID")]
+    public int? SectionID { get; set; }
+
+    [StringLength(50)]
+    public string? Position { get; set; }
+
+    public string? Description { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? Quantity { get; set; }
+
+    [StringLength(50)]
     public string? Unit { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? SupplyPerUnit { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? InstallationPerUnit { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? TotalPerUnit { get; set; }
+
+    [Column(TypeName = "decimal(10,2)")]
     public decimal? Total { get; set; }
-    public int SortOrder { get; set; }
+
+    public int? SortOrder { get; set; }
+
+
 
     // Navigation property
     public ProjectSection Section { get; set; } = null!;
